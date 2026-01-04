e920d382027c9b3ca9a5b0c8767771fb665ad7f5
xistoh162108 <xistoh162108@gmail.com>
2026-01-04 12:33:25 +0900

    Chore: 필요없는 파일 제거

--- check_stdout.py
+++
@@ -1,7 +0,0 @@
-import sys
-original_stdout = sys.stdout
-import kss
-if sys.stdout != original_stdout:
-    print(f"KSS replaced sys.stdout with {sys.stdout}")
-else:
-    print("KSS did not replace sys.stdout")
